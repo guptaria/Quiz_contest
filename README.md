482c7fdc37341cf56156e032e7e9551ebde0b980
Priya Garg <gupta.ria100@gmail.com>
2020-11-06 19:09:50 -0600

    adding comments and readme

--- README.md
+++ README.md
@@ -1,37 +1,28 @@
-# Description
+# Quiz_Contest
 
-This is a quiz application using HTML, CSS, and Javascript. This application emphasizes the use of Javascript to provide quiz questions and collect user data to determine whether the answers to a question are correct, this then generates a score and appends that into the scores data.
+## Description
+This is a quiz application using HTML, CSS, and Javascript. This application emphasizes the use of Javascript to provide quiz questions and collect user data to determine whether the answers to a question are correct, this then generates a score and appends that into the scores data.The timer starts When you click the "Start Quiz". Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty and a lower score.HTML and CSS and Javascript documents create a quiz with multiple choice questions with Javascript trivia.
 
-Installing
-To install this code, download the zip file, or use GitHub's guidelines to clone the repository.
+## Table of Contents 
 
-Summary
-HTML and CSS and Javascript documents create a quiz with multiple choice questions with Javascript trivia
-This project emphasizes the use of using Javascript to make dynamic changes to an HMTL document
-This project utilizes the use of appending HTML pages
-This project has the following features:
-A Start Quiz button
-This starts a timer for the user
-Total time for the Quiz is 60 Sec
-An appended HTML page that features questions, and multiple choice answers
-If questions are answered incorrectly, 10 seconds are deducted off remaining time
-Answers are recording using an event listener, "click" and tracks correct answers
+* [Installation](#Installation)
+* [Technologies Used](#Technology)
+* [Psuedo code](#Psuedocode)
+* [Summary](#Summary)
+* [Tests](#Tests)
+* [ResourceReffered](#ResourceReffered)
+* [Github_Links](#Github_Links)
 
 
+## Installation
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository., Links are provided below in resources.
 
+## Technologies Used
+JavaScript
+HTML
+CSS
 
-An appended HTML page that features:
-Final score which is calculated using time remaining
-Input area to record initials.
-A Submit score button.
-Submit score buttom saves initials and score to local storage
-
-
-A Highscores HTML
-This a list summary of intials and final scores
-
-
-Psuedo code:
+## Psuedo code
 Create a timer attached to a start button with a starting value of 60.
 When start button  is pressed start a reverse countdown
 When countdown starts, start quiz
@@ -65,14 +56,36 @@ Adjusts buttons to be centered and stacked
 To Execute File:
 Open in browser
 
-Features:
-Two HTML Pages
-Index.html
-Contains landing page to start timer
-Appends two new pages
-Highscores * Retreives local data from previous page
-One CSS Page
-Styles.css
-Contains centering and styling for html list features
-Contains media queries
-Two Javascript Page * Contains: * Variables, including arrays with object * Event listeners * if/else if statements * For Loops * Functions * Local Storage set and get
+## Summary
+This project emphasizes the use of using Javascript to make dynamic changes to an HMTL document
+This project utilizes the use of appending HTML pages
+This project has the following features:
+A Start Quiz button
+This starts a timer for the user
+Total time for the Quiz is 60 Sec
+An appended HTML page that features questions, and multiple choice answers
+If questions are answered incorrectly, 10 seconds are deducted off remaining time
+Answers are recording using an event listener, "click" and tracks correct answers.
+
+## Tests 
+### ScreenShots
+This is the main page screen.
+![](assets/screenshot1.png)
+When you get the answer correct.
+![](assets/screenshot2.png)
+When you get the answer wrong.
+![](assets/screenshot3.png)
+Game is over, either its time over or its out of questions.
+![](assets/screenshot4.png)
+Submitting your initials to get the scores stored.
+![](assets/screenshot5.png)
+![](assets/screenshot6.png)
+
+## ResourceReffered
+[For Time Interval] https://www.w3schools.com/jsref/met_win_setinterval.asp
+[For Quiz concepts] https://stackoverflow.com/questions/3275164/javascript-quiz
+
+## Github_Links
+[**URL of My Github Repository**]()
+[**URL of Deployed Application**]()
+
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// getting all the elements from DOM and storing it to local variables
 var startEl=document.querySelector("#start");
 var quizEl=document.querySelector("#quiz");
 var hideEl=document.querySelector(".hide");
@@ -11,18 +12,19 @@ var introEl=document.querySelector("#intro");
 var messageEl=document.querySelector("#message");
 
 var highScoresEl=document.querySelector("#highScores");
-var currentPosition=0;
+var currentPosition=0;   
 var scores=0;
-timeleft=60;
+timeleft=60;  // initialising the time to 60 sec
 var timeOver;
 
-var highscores= JSON.parse(localStorage.getItem("scores")) || []
+var highscores= JSON.parse(localStorage.getItem("scores")) || [] // getting the value from local storage and converting string to object
 
+// questions are stored in array of objects
 var questions =[
 
-    {   question:"1. What is the HTML tag under which one can write the JavaScript code?",
-        choices:["A. javascript","B. scripted","C. script","D. js"],
-        answer:"C. script"
+    {   question:"1. What is the HTML tag under which one can write the JavaScript code?", //question
+        choices:["A. javascript","B. scripted","C. script","D. js"],       // 4 choices to choose from
+        answer:"C. script"   // correct answer
     },
 
     {
@@ -53,6 +55,7 @@ var questions =[
     }
 ];
 
+// adding Event Listener on click
 startEl.addEventListener("click",countDown);
 buttonOneEl.addEventListener("click",checkAnswer);
 buttonTwoEl.addEventListener("click",checkAnswer);
@@ -60,27 +63,30 @@ buttonThreeEl.addEventListener("click",checkAnswer);
 buttonFourEl.addEventListener("click",checkAnswer);
 
 
-
+//Function for timer (set interval)
 function countDown()
 {
-    quizEl.classList.remove("hide");
+    quizEl.classList.remove("hide");    // removing hide class so that it will be visible now
      introEl.innerHTML="";
     timeInterval = setInterval(function()
     {
-        timeEl.textContent=timeleft +" seconds remaining";
-        timeleft--;
+        timeEl.textContent=timeleft +" seconds remaining";   // showing time on screen 
+        timeleft--;     // decrementing time
         if(timeleft<=0)
         {
         gameOver();
         }
-    },1000);
+    },1000);  // setting interval 1000millisec
+    
 }
+//loading questions
     quizTextEl.textContent = questions[0].question;
     buttonOneEl.innerHTML = questions[0].choices[0];
     buttonTwoEl.innerHTML = questions[0].choices[1];
     buttonThreeEl.innerHTML = questions[0].choices[2];
     buttonFourEl.innerHTML = questions[0].choices[3];
 
+    // execute when out of questions or time over
     function gameOver(){
         clearInterval(timeInterval);
         timeEl.textContent="Game Over";
@@ -89,32 +95,30 @@ function countDown()
     }
    
 
-
+// to check the answer
 function checkAnswer(event)
 {
-    if(event.target.innerHTML === questions[currentPosition].answer)
+    if(event.target.innerHTML === questions[currentPosition].answer) // if ans is correct
     {
-       messageEl.innerHTML="Yay!!! You have answered this question Correctly. "
+       messageEl.innerHTML="Yay!!! You have answered this question Correctly. " // message to display if ans is correct
        messageEl.style.color="green";
-       setTimeout (nextQuestion,1000);
+       setTimeout (nextQuestion,10000);
        
     }
     else{
-        timeleft=timeleft-10;
+        timeleft=timeleft-10; // if ans is wron deducting 10 sec from timer
         messageEl.innerHTML="Sorry!! This is the Wrong. Correct ans is : "+questions[currentPosition].answer;
-        messageEl.style.color="red";
-        setTimeout (nextQuestion,1000);
+        messageEl.style.color="red";   // change color to red if ans wrong
+        setTimeout (nextQuestion,10000);
         }
         
 }
-    
+    // function to load next question
 function nextQuestion()
 {
-    // event.target.style.backgroundColor='';
-messageEl.style.color="white";
-currentPosition++;
+currentPosition++; // incrementing the index of questions
 console.log("question.length is" +questions.length);
-if(currentPosition==questions.length)
+if(currentPosition==questions.length)  // 
 {
     console.log("out of ques");
     gameOver();
@@ -131,21 +135,21 @@ buttonFourEl.innerHTML= questions[currentPosition].choices[3];
 
 
 document.querySelector("#scoreSubmit").addEventListener("click",function(event){
-    var userName=document.querySelector("#userName").value;
+    var userName=document.querySelector("#userName").value; //getting username from user
     document.querySelector("#scoreSubmit").classList.add("hide");
     document.querySelector("#userName").classList.add("hide");
     document.querySelector("#scoreList").classList.remove("hide")
-    var thisScore = {
-        name:userName,
+    var thisScore = {   //moving the  scores to username
+        name:userName,  
         score:timeleft
     }
 
-    highscores.sort(function(a, b){return a-b});
-    highscores.push(thisScore);
-    localStorage.setItem("scores",JSON.stringify(highscores))
+    highscores.sort(function(a, b){return a-b});  // sorting the scores from high to low
+    highscores.push(thisScore); // pushing the scores to highscores
+    localStorage.setItem("scores",JSON.stringify(highscores))  // changing object to string to store in local storage
 
-    for(score of highscores){
-        document.querySelector("#scoreList").innerHTML+=`<tr>
+    for(score of highscores){    // displaying the scores and username.
+        document.querySelector("#scoreList").innerHTML+=`<tr>   
         
         <td>${score.name}</td>
         <td>${score.score}</td>
